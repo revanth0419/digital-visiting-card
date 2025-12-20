d593a04166b25df10cbd03afe5d6cee71276392e
Kasireddy Revanth <revanthofficial04@gmail.com>
2025-12-20 17:15:35 +0530

    refactor: remove Earn section files

--- src/App.tsx
+++ src/App.tsx
@@ -53,16 +53,7 @@ const AnimatedRoutes = () => {
             <Dashboard />
           </motion.div>
         } />
-        {/* <Route path="/dashboard/earn" element={
-          <motion.div
-            initial={{ opacity: 0, x: 20 }}
-            animate={{ opacity: 1, x: 0 }}
-            exit={{ opacity: 0, x: -20 }}
-            transition={{ duration: 0.3 }}
-          >
-            <Earn />
-          </motion.div>
-        } /> */}
+
         <Route path="/u/:username" element={
           <motion.div
             initial={{ opacity: 0, scale: 0.95 }}
